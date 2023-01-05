8c1fd9eee0571a463eb0ec777de2398a5056fcff
niewenlong <niewenlong@corp.netease.com>
2023-01-05 17:46:21 +0800

    [leetCode]BM6 判断链表中是否有环 https://www.nowcoder.com/practice/650474f313294468a4ded3ce0f7898b9?tpId=295&tqId=605&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj

---
+++ top101/bm6/bm6.go
@@ -0,0 +1,34 @@
+/*
+author niewenlong
+date 2023/1/5 17:36
+description BM6 判断链表中是否有环
+leetCodeUrl https://www.nowcoder.com/practice/650474f313294468a4ded3ce0f7898b9?tpId=295&tqId=605&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj
+*/
+
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	var slow = head
+	var fast = head.Next
+
+	for slow != nil && fast != nil && fast.Next != nil {
+		if slow == fast {
+			return true
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return false
+}
+
+func main() {
+	hasCycle(nil)
+}
