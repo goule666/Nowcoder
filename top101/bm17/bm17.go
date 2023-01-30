685ff10fd68e505fe1caa8f70ee9741d0e8826d3
niewenlong <niewenlong@corp.netease.com>
2023-01-30 17:56:31 +0800

    [leetCode]BM17 二分查找-I https://www.nowcoder.com/practice/d3df40bd23594118b57554129cadf47b?tpId=295&tqId=1499549&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj

---
+++ top101/bm17/bm17.go
@@ -0,0 +1,38 @@
+/*
+author niewenlong
+date 2023/1/30 17:25
+description BM17 二分查找-I
+leetCodeUrl https://www.nowcoder.com/practice/d3df40bd23594118b57554129cadf47b?tpId=295&tqId=1499549&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj
+*/
+
+package main
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param nums int整型一维数组
+ * @param target int整型
+ * @return int整型
+ */
+func search(nums []int, target int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	println(search([]int{1, 2, 3, 4, 5}, 6))
+}
